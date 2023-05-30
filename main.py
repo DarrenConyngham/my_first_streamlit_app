34dea5eb252570dd1df14522e6564f157bc30d9b
Darren Conyngham <dconyngham92@gmail.com>
2023-05-30 13:57:10 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Our first Streamlit app")
+st.subheader("Introducing Streamlit in Automate Everything with Python")
+st.write("This is to demonstrate how to use Streamlit to build a simple web app.")
